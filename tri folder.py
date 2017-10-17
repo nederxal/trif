d90c8d902051de19021e3f0479a0bbbf922a1e23
nederxal <adelanchy@protonmail.ch>
2017-10-17 14:26:23 +0200

    Modifs
    - Ajout d'un dictionnaire pour création folder + patterns pour trier
    - Refonte de la boucle pour ranger
    - Utilisation os.path pour des chemins propres

--- tri folder.py
+++ tri folder.py
@@ -7,54 +7,49 @@ import os
 import sys
 import re
 
-images = "jpg|jpeg|bpm|png"
-videos = "avi|mkv|mp4"
-exe = "exe|sh|ksh|msi"
-docs = "docx|xlsx|pptx|doc|xls|ppt|pdf"
-texte = "txt"
-divers = "iso|cue|zip|rar|7z"
+dicTri={\
+    "IMAGES":"jpg|jpeg|bpm|png",\
+    "VIDEOS":"avi|mkv|mp4",\
+    "EXEC":"exe|sh|ksh|msi",\
+    "DOCUMENTS":"docx|xlsx|pptx|doc|xls|ppt|pdf",\
+    "TEXTE":"txt",\
+    "DIVERS":"iso|cue|zip|rar|7z"
+    }
 
-download = os.environ['HOME']+'/Downloads'
-tri = os.environ['HOME']+"/A_TRIER/"
-repTri = ['IMAGES', 'VIDEOS', 'EXECUTABLES', 'DOCUMENTS', 'TEXTE', 'DIVERS']
+download = os.path.join(os.environ['HOME'],'Downloads')
+tri = os.path.join(os.environ['HOME'],'A_TRIER')
+
 notSorted = []
 
 if not os.path.isdir(tri):
     try:
-        os.mkdir(os.environ['HOME']+'/A_TRIER')
-    except OSError as e:
-        print (e)
-        raise
+        os.mkdir(os.path.join(os.environ['HOME'],'A_TRIER'))
+    except FileExistsError as e:
+            if e.errno != 17:
+                print(e)
+                raise
 
-for rep in repTri:
+for rep in dicTri.keys():
     if not os.path.isdir(tri+rep):
         try:
-            os.mkdir(os.environ['HOME']+"/A_TRIER/"+rep)
-        except OSError as e:
-           print (e)
-           raise
+            os.mkdir(os.path.join(tri,rep))
+        except FileExistsError as e:
+            if e.errno != 17:
+                print(e)
+                raise
 
 with os.scandir(download) as it:
     for ent in it:
         if not ent.name.startswith('.') \
            and not ent.name.endswith('ini')\
            and ent.is_file():
-            if re.search(images, ent.name, re.IGNORECASE):
-                os.replace(ent.path, tri+repTri[0]+"/"+ent.name)
-            elif re.search(videos, ent.name, re.IGNORECASE):
-                os.replace(ent.path, tri+repTri[1]+"/"+ent.name)
-            elif re.search(exe, ent.name, re.IGNORECASE):
-                os.replace(ent.path, tri+repTri[2]+"/"+ent.name)
-            elif re.search(docs, ent.name, re.IGNORECASE):
-                os.replace(ent.path, tri+repTri[3]+"/"+ent.name)
-            elif re.search(texte, ent.name, re.IGNORECASE):
-                os.replace(ent.path, tri+repTri[4]+"/"+ent.name)
-            elif re.search(divers, ent.name, re.IGNORECASE):
-                os.replace(ent.path, tri+repTri[5]+"/"+ent.name)
-            else:
-                notSorted.append(ent.name)
+                for folder,extens in dicTri.items():
+                    if re.search(extens, ent.name, re.IGNORECASE):
+                        os.replace(ent.path, os.path.join(tri,folder,ent.name))
         elif not ent.name.startswith('.') and ent.is_dir():
             notSorted.append("DOSSIER : "+ent.name)
+        else:
+            notSorted.append(ent.name)
 
 print("Les choses suivantes ne sont pas triées : \n")
 print('\n'.join(notSorted))
